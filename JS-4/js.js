729dcf337053f3fc8123ed901068c71f491e9f53
MaxwelTbz1 <registbz22@gmail.com>
2024-11-26 11:56:40 +0100

    first commit

---
+++ JS-4/js.js
@@ -0,0 +1,15 @@
+let age = 25; 
+let hasLicense = true; 
+
+if(age >= 16){
+    console.log("You are old enough to drive");
+
+    if(hasLicense){
+        console.log("You have your license!");
+    } 
+    else {
+        console.log("You do not have your license  yet!");
+    }
+} else {
+    console.log("You must be 16+ to have a license");
+}
